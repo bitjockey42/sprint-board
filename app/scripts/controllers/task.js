02d41ca94ca482dee07da06d70063a557826e468
hckr <hckr@eml.cc>
2014-01-26 10:53:33 -0600

    Add TaskCtrl.

---
+++ app/scripts/controllers/task.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kanbanBoardApp')
+  .controller('TaskCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
