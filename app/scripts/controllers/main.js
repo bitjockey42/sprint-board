28ea56df4f90dba30cb3cc2cbc87cb60a94a0069
hckr <hckr@eml.cc>
2014-01-26 10:22:03 -0600

    Bootstrap files.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kanbanBoardApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
