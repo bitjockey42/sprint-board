e6813687013c5e68451b3923bae640328590c1fa
hckr <hckr@eml.cc>
2014-01-26 10:42:34 -0600

    Created a new controller.

---
+++ app/scripts/controllers/board.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kanbanBoardApp')
+  .controller('BoardCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
