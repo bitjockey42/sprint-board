61fd222176b122aad99a5563e3fdb51138272b66
AJ <ally@26s.co>
2014-05-09 13:30:19 -0500

    Move tag stuff into points controller.

--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -2,29 +2,5 @@
 
 angular.module('kanbanBoardApp')
   .controller('BoardCtrl', ['$scope', 'Task', 'Project', 'Workspace', 'Tag', 'WORKSPACE_ID', 'PROJECT_ID', function ($scope, Task, Project, Workspace, Tag, WORKSPACE_ID, PROJECT_ID) {
-    $scope.tags = [];
-    var tagsLoaded = false;
 
-    $scope.tagsLoaded = function () {
-      return tagsLoaded;
-    };
-
-    $scope.loadTags = function () {
-      Workspace.get({path: 'tags'}, function (response) {
-        $scope.tags = response.data;
-        tagsLoaded = true;
-      });
-    };
-    
-    $scope.getPointTags = function (tags) {
-      return tags.filter( function (tag) {
-        if (tag.name.match(/points/)) {
-          return tag;
-        }
-      });
-    };
-    
-    $scope.tagPointValue = function (tag) {
-      return parseInt(tag.name.replace(/[^\d+]/g, ''));
-    }
   }]);
--- app/scripts/controllers/points.js
+++ app/scripts/controllers/points.js
@@ -2,5 +2,29 @@
 
 angular.module('kanbanBoardApp')
   .controller('PointsCtrl', ['$scope', 'Workspace', 'Tag', 'WORKSPACE_ID', function ($scope, Workspace, Tag, WORKSPACE_ID) {
-    $scope.tags = Workspace.get({path: 'tags'});  
+    $scope.tags = [];
+    var tagsLoaded = false;
+
+    $scope.tagsLoaded = function () {
+      return tagsLoaded;
+    };
+
+    $scope.loadTags = function () {
+      Workspace.get({path: 'tags'}, function (response) {
+        $scope.tags = response.data;
+        tagsLoaded = true;
+      });
+    };
+    
+    $scope.getPointTags = function (tags) {
+      return tags.filter( function (tag) {
+        if (tag.name.match(/points/)) {
+          return tag;
+        }
+      });
+    };
+    
+    $scope.tagPointValue = function (tag) {
+      return parseInt(tag.name.replace(/[^\d+]/g, ''));
+    }
   }]);
