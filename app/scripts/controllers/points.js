1c4b31fe4bc3430068cb7be83b41d1e2fb98c4c4
AJ <ally@26s.co>
2014-05-09 14:06:08 -0500

    Show point value for each task.

--- app/scripts/controllers/points.js
+++ app/scripts/controllers/points.js
@@ -15,7 +15,16 @@ angular.module('kanbanBoardApp')
         tagsLoaded = true;
       });
     };
-    
+
+    $scope.taskPoints = function (tags) {
+      var pointTags = $scope.getPointTags(tags);
+      var pointValue = 0;
+      if (pointTags.length > 0) {
+        pointValue = $scope.tagPointValue(pointTags[0]);
+      }
+      return pointValue;
+    };
+
     $scope.getPointTags = function (tags) {
       return tags.filter( function (tag) {
         if (tag.name.match(/points/)) {
