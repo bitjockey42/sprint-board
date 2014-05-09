99d2a2b9087a206f2fc8f828ce59e03cbefc2bf8
AJ <ally@26s.co>
2014-05-09 15:49:10 -0500

    Show point tag choices.

--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -4,16 +4,34 @@ angular.module('kanbanBoardApp')
   .controller('BoardCtrl', ['$scope', 'Task', 'Project', 'Workspace', 'Tag', 'WORKSPACE_ID', 'PROJECT_ID', function ($scope, Task, Project, Workspace, Tag, WORKSPACE_ID, PROJECT_ID) {
     $scope.pointsTotal = [];
     $scope.tasks = [];
+    $scope.tags = [];
     var tasksLoaded = false;
+    var tagsLoaded = false;
 
     $scope.tasksLoaded = function () {
       return tasksLoaded;
     }
 
+    $scope.init = function () {
+      $scope.loadProjectTasks();
+      $scope.loadTags();
+    };
+
     $scope.loadProjectTasks = function () {
       Project.tasks({projectId: PROJECT_ID}, function (response) {
         $scope.tasks = response.data;
         tasksLoaded = true;
+      });
+    };
+
+    $scope.tagsLoaded = function () {
+      return tagsLoaded;
+    };
+
+    $scope.loadTags = function () {
+      Workspace.get({path: 'tags'}, function (response) {
+        $scope.tags = response.data;
+        tagsLoaded = true;
       });
     };
 
