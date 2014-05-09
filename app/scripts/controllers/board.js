0b2bfdb1985dc6aefe1875e9a610217f3ed4975a
AJ <ally@26s.co>
2014-05-09 13:34:52 -0500

    Load project tasks in the board.

--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -2,5 +2,18 @@
 
 angular.module('kanbanBoardApp')
   .controller('BoardCtrl', ['$scope', 'Task', 'Project', 'Workspace', 'Tag', 'WORKSPACE_ID', 'PROJECT_ID', function ($scope, Task, Project, Workspace, Tag, WORKSPACE_ID, PROJECT_ID) {
+    $scope.tasks = [];
+    var tasksLoaded = false;
+
+    $scope.tasksLoaded = function () {
+      return tasksLoaded;
+    }
+
+    $scope.loadProjectTasks = function () {
+      Project.tasks({projectId: PROJECT_ID}, function (response) {
+        $scope.tasks = response.data;
+        tasksLoaded = true;
+      });
+    };
 
   }]);
